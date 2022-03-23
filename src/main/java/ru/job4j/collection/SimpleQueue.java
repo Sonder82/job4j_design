1105ec63f888880289b2924ef1d7e0b378f9dc27
Sonder82 <Himrs82@gmail.com>
2022-03-24 00:49:51 +0500

    5. Очередь на двух стеках [#160]

---
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -0,0 +1,35 @@
+package ru.job4j.collection;
+
+/**
+ * Класс описывает работу используя очередь Queue.
+ * Применяем "очередь на двух стеках"
+ * FIFO first input first output(первый пришел, первый ушел)
+ * @param <T>
+ */
+public class SimpleQueue<T> {
+    private final SimpleStack<T> in = new SimpleStack<>();
+    private final SimpleStack<T> out = new SimpleStack<>();
+
+    /**
+     * Метод возвращает первое значение и удаляет его из коллекции.
+     * Последовательно перекладывать элементы из стек in в out.
+     * @return возвращаем, что положили первым
+     */
+    public T poll() {
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
+        return out.pop();
+    }
+
+    /**
+     * Метод помещает значение в конец.
+     * Помещаем в стек in.
+     * @param value значение.
+     */
+    public void push(T value) {
+        in.push(value);
+    }
+}
--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -10,21 +10,36 @@ public class SimpleStack<T> {
     private ForwardLinked<T> linked = new ForwardLinked<>();
 
     /**
+     * размер списка
+     */
+    private int size;
+
+    /**
      * Метод возвращает значение и удаляет его из коллекции.
-     *
+     * При этом уменьшаем размер коллекции.
      * @return возвращает значение.
      */
     public T pop() {
+        size--;
         return linked.deleteFirst();
     }
 
 
     /**
      * Метод принимает значение и возвращает его в коллекцию.
-     *
+     * Увеличиваем размер коллекции.
      * @param value значение
      */
     public void push(T value) {
         linked.addFirst(value);
+        size++;
+    }
+
+    /**
+     * Метод проверяет размер коллекции(пустая или нет).
+     * @return
+     */
+    public boolean isEmpty() {
+        return size == 0;
     }
 }
