652281995f28bba6875acdbd71c8e0f6eb528b6d
Sonder82 <Himrs82@gmail.com>
2022-04-26 19:37:49 +0500

    4.2. Поиск дубликатов [#315066 #293842]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,15 +9,25 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".txt"))
-                .forEach(System.out::println);
+        if (validation(args)) {
+            search(Paths.get(args[0]), p -> p.toFile().getName().endsWith(args[1]))
+                    .forEach(System.out::println);
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(".txt"));
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static boolean validation(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Root folder and file extension are required. "
+                    + "Usage java -jar dir.jar ROOT_FOLDER FILE_EXTENSION.");
+        }
+       // if ()
+        return true;
     }
 }
 
