1e0f08cec3ff9b9688c3e63e83cd8aee4dbe6996
Sonder82 <Himrs82@gmail.com>
2022-05-18 14:28:24 +0500

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,18 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Aleksey Novoselov";
+        int age = 40;
+        char sex = 'M';
+        double height = 1.75;
+        boolean permit = true;
+        LOG.debug("User info name : {}, age : {}, sex : {}, height : {}, job permit : {}", name, age, sex, height, permit);
+        float pi = 3.14f;
+        LOG.debug("Number PI is :  {}", pi);
+        byte file = 25;
+        LOG.debug("Size file is : {}", file);
+        long distance = 149597870700L;
+        LOG.debug("Distance to Sun at meters : {}", distance);
+
     }
 }
