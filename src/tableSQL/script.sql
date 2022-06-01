7915bbc96fe8ab834c7b7bdef8318dbc3b8fd8a2
Sonder82 <Himrs82@gmail.com>
2022-06-01 14:31:28 +0500

    0. Что такое база данных. [#6860 #307819]

--- src/tableSQL/script.sql
+++ src/tableSQL/script.sql
@@ -1,3 +1,10 @@
+create table person(
+	id serial primary key,
+	name varchar(255),
+	region text,
+	age int,
+	permission bool
+);
 insert into person(name, region, age, permission) values('Aleksey', 'Ekaterinburg', '40', 'true');
 update person set region = 'Moscow';
 delete from person;
