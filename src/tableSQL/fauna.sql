9992171dfbfd8a1c84934bfc2c9810c1146f5063
Sonder82 <Himrs82@gmail.com>
2022-06-05 17:25:27 +0500

    0. Select [#6861]

---
+++ src/tableSQL/fauna.sql
@@ -0,0 +1,23 @@
+create table fauna (
+    id serial primary key,
+    name text,
+    avg_age int,
+    discovery_date date
+);
+
+insert into fauna(name, avg_age, discovery_date)
+values ('tiger', 30, date '1797-06-22');
+insert into fauna(name, avg_age, discovery_date)
+values ('wolf', 25, date '1421-03-12');
+insert into fauna(name, avg_age, discovery_date)
+values ('bear', 54, date '878-11-11');
+insert into fauna(name, avg_age, discovery_date)
+values ('fish', 11547, null);
+
+select * from fauna where name like 'fish';
+select avg_age from fauna where avg_age > 10000 and avg_age < 21000;
+select * from fauna where discovery_date is null;
+select name,discovery_date from fauna where discovery_date < '1950-01-01';
+
+
+
---
+++ src/tableSQL/students.sql
@@ -0,0 +1,6 @@
+select name,budget from students;
+select * from students where course = 3;
+select * from students where budget = true and speciality like '%2';
+select * from students where budget = true or speciality like '%2';
+select current_date;
+select * from students order by name asc;
