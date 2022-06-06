ded65c4a089d69357f5989ca925c304e99245fcd
Sonder82 <Himrs82@gmail.com>
2022-06-06 12:12:39 +0500

    0. Select [#6861 #309257]

--- src/tableSQL/fauna.sql
+++ src/tableSQL/fauna.sql
@@ -14,7 +14,7 @@ values ('bear', 54, date '878-11-11');
 insert into fauna(name, avg_age, discovery_date)
 values ('fish', 11547, null);
 
-select * from fauna where name like 'fish';
+select * from fauna where name like '%fish%';
 select avg_age from fauna where avg_age > 10000 and avg_age < 21000;
 select * from fauna where discovery_date is null;
 select name,discovery_date from fauna where discovery_date < '1950-01-01';
