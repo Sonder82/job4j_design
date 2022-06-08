1bc713da9de2bfb17344cf2e9adf39fb52d3c545
Sonder82 <Himrs82@gmail.com>
2022-06-09 00:06:32 +0500

    3. Фильтры [#1732 #310273]

--- src/tableSQL/product.sql
+++ src/tableSQL/product.sql
@@ -17,13 +17,19 @@ insert into product(name, type_id, expired_data, price) values ('Сыр плав
 insert into product(name, type_id, expired_data, price) values ('Молоко топленое', 2, '2022-07-11', 65),
 ('Сливочное мороженое', 2, '2022-12-21', 150),('Сливки', 2, '2022-09-01', 110),('Молочный коктейль', 2, '2022-06-01', 100);
 
-select * from product where type_id = 1;
+select t.name, p.name
+from product as p 
+join type t
+on p.type_id = t.id
+where t.name = 'Сыр';
 
 select * from product where name like '%мороженое%';
 
 select * from product where expired_data < current_date;
 
-select max(price) from product;
+select name, price
+from product  
+where price = (select max(price) from product);
 
 select t.name, count(p.type_id)
 from product as p
@@ -31,7 +37,11 @@ join type t
 on p.type_id = t.id
 group by t.name;
 
-select * from product where type_id = 1 or type_id = 2;
+select t.name, p.name
+from product as p 
+join type t
+on p.type_id = t.id
+where t.name = 'Сыр' or t.name = 'Молоко';
 
 select t.name, count(p.type_id)
 from product as p
