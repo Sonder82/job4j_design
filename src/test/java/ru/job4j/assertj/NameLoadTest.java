c3a2853c3836a9eda6878cf1fba12d554321ef54
Sonder82 <Himrs82@gmail.com>
2022-08-15 19:12:47 +0500

    Утверждения с исключениями [#504886]

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,63 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+class NameLoadTest {
+
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkNamesWithFirstEqually() {
+        NameLoad nameLoad = new NameLoad();
+        String names = "=";
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("^.+")
+                .hasMessageContaining(names)
+                .hasMessageContaining("key");
+
+    }
+
+    @Test
+    void checkNamesWithLastEqually() {
+        NameLoad nameLoad = new NameLoad();
+        String names = "one=";
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("^.+")
+                .hasMessageContaining(names)
+                .hasMessageContaining("value");
+
+    }
+
+    @Test
+    void checkNamesWithoutEqually() {
+        NameLoad nameLoad = new NameLoad();
+        String names = "one";
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("^.+")
+                .hasMessageContaining(names)
+                .hasMessageContaining("symbol");
+
+    }
+
+    @Test
+    void checkEmptyNames() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::parse)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("empty");
+    }
+}
