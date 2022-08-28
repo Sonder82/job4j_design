1800a3674e3989e44f39d43b477470be9fc1c946
Sonder82 <Himrs82@gmail.com>
2022-08-28 09:49:28 +0500

    1. Представления [#504792 #336909]

---
+++ src/tableSQL/view/main.sql
@@ -0,0 +1,54 @@
+create table authors (
+    id serial primary key,
+    name varchar(50)
+);
+
+insert into authors (name) values ('Александр Пушкин');
+insert into authors (name) values ('Николай Гоголь');
+
+create table books (
+    id serial primary key,
+    name varchar(200),
+    author_id integer references authors(id)
+);
+
+insert into books (name, author_id) values ('Евгений Онегин', 1);
+insert into books (name, author_id) values ('Капитанская дочка', 1);
+insert into books (name, author_id) values ('Дубровский', 1);
+insert into books (name, author_id) values ('Мертвые души', 2);
+insert into books (name, author_id) values ('Вий', 2);
+
+create table orders (
+    id serial primary key,
+    active boolean default true,
+    book_id integer references books(id),
+    student_id integer references students(id)
+);
+
+insert into orders (book_id, student_id) values (1, 1);
+insert into orders (book_id, student_id) values (3, 1);
+insert into orders (book_id, student_id) values (5, 2);
+insert into orders (book_id, student_id) values (4, 1);
+insert into orders (book_id, student_id) values (2, 2);
+
+create table students (
+    id serial primary key,
+    name varchar(50)
+);
+
+insert into students (name) values ('Иван Иванов');
+insert into students (name) values ('Петр Петров');
+
+create view show_student_with_max_count_orders
+    as select s.name as student, count(o.id) as count_orders
+	     from students as s
+         join orders o on s.id = o.student_id
+         group by (s.name) having count(o.id) = (
+			 select max(max_count_orders)
+			 from (
+		 select s.name as student, count(o.id) as max_count_orders
+	     from students as s
+         join orders o on s.id = o.student_id
+			 group by s.name) foo);
+
+select * from show_student_with_max_count_orders;
