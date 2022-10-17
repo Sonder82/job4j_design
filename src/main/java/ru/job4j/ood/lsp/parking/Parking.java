f69030dd6dc8b68d902daf8b16f6d3e27154d32f
Sonder82 <Himrs82@gmail.com>
2022-10-17 18:55:00 +0500

    2. Парковка машин [#853]

--- src/main/java/ru/job4j/ood/lsp/parking/Parking.java
+++ src/main/java/ru/job4j/ood/lsp/parking/Parking.java
@@ -1,5 +1,8 @@
 package ru.job4j.ood.lsp.parking;
 
+import java.util.List;
+
 public interface Parking {
-    void add (Automobile automobile);
+    void add(Automobile automobile);
+    List<Automobile> getAll();
 }
--- src/main/java/ru/job4j/ood/lsp/parking/PlaceForLorry.java
+++ src/main/java/ru/job4j/ood/lsp/parking/PlaceForLorry.java
@@ -1,8 +1,15 @@
 package ru.job4j.ood.lsp.parking;
 
+import java.util.List;
+
 public class PlaceForLorry implements Parking {
     @Override
     public void add(Automobile automobile) {
 
+    }
+
+    @Override
+    public List<Automobile> getAll() {
+        return null;
     }
 }
