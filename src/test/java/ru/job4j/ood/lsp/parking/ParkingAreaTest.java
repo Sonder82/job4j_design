7f0f836a31495e57f325e3f902601d24305ede8f
Sonder82 <Himrs82@gmail.com>
2022-10-19 18:50:19 +0500

    2. Парковка машин [#853 #352529]

--- src/test/java/ru/job4j/ood/lsp/parking/ParkingAreaTest.java
+++ src/test/java/ru/job4j/ood/lsp/parking/ParkingAreaTest.java
@@ -1,11 +1,13 @@
 package ru.job4j.ood.lsp.parking;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 class ParkingAreaTest {
 
+    @Disabled
     @Test
     void addTwoCarsAndOneTruck() {
         ParkingArea parkingArea = new ParkingArea(2, 1);
